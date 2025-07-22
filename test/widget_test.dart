a0b9243bafc0b23176fe2c1b40a8e13537ac8697
Dilshad Alam <rayeendilshan@gmail.com>
2025-07-23 01:08:05 +0530

    TDD for all the 5 cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,34 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:incubyte_assement/string_calculator.dart';
 
-import 'package:incubyte_assement/main.dart';
+void main() {
+  group('StringCalculator.add()', () {
 
-void main() {}
+    test('Case 1: comma-separated numbers (1,5) returns 6', () {
+      expect(StringCalculator.add("1,5"), equals(6));
+    });
+
+    test('Case 2: handles any amount of numbers (1,5,2,3,4) returns 15', () {
+      expect(StringCalculator.add("1,5,2,3,4"), equals(15));
+    });
+
+    test('Case 3: handles newlines (1\n2,3) returns 6', () {
+      expect(StringCalculator.add("1\n2,3"), equals(6));
+    });
+
+    test('Case 4: handles custom delimiter (//;\n1;2) returns 3', () {
+      expect(StringCalculator.add("//;\n1;2"), equals(3));
+    });
+
+    test('Case 5: throws exception for negatives (1,-2)', () {
+      expect(
+        () => StringCalculator.add("1,-2"),
+        throwsA(
+          predicate(
+            (e) => e.toString().contains("Negative numbers not allowed: -2"),
+          ),
+        ),
+      );
+    });
+  });
+}
